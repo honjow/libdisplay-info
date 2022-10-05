5b81e0b578e6d6e2bb17ad2999090ad6168f388b
Simon Ser <contact@emersion.fr>
2022-10-05 09:58:00 +0200

    di-edid-decode: add special case for 16:10 aspect ratio
    The mathematical aspect ratio is 8:5 but people use 16:10.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- di-edid-decode/edid.c
+++ di-edid-decode/edid.c
@@ -114,6 +114,11 @@ compute_aspect_ratio(int width, int height, int *horiz_ratio, int *vert_ratio)
 		*horiz_ratio = width / d;
 		*vert_ratio = height / d;
 	}
+
+	if (*horiz_ratio == 8 && *vert_ratio == 5) {
+		*horiz_ratio = 16;
+		*vert_ratio = 10;
+	}
 }
 
 /**
